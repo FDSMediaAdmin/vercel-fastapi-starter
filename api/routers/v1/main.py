5b682fae8199c4aea36e5195e02093220d934208
Frank-Dieter Stadtler <development@fds-media.co>
2024-06-19 18:05:56 +0800

    featured(database): Added verifying a request is from rapidapi if enabled

--- api/routers/v1/main.py
+++ api/routers/v1/main.py
@@ -1,10 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Header, HTTPException
 from fastapi_versioning import version
 
 from loguru import logger
+from typing_extensions import Annotated
+
+from shared.decorator.rapidapi.header_checker import header_check
+
+from api.config.config import settings
 
 router = APIRouter(prefix='/main')
+# 'Content-Type'
+def is_request_from_rapid_api(x_rapidapi_host: Annotated[str, Header()],
+                                    x_rapidapi_version: Annotated[str, Header()],
+                                    x_rapidapi_request_id: Annotated[str, Header()],
+                                    x_rapidapi_user: Annotated[str, Header()],
+                                    x_rapidapi_subscription: Annotated[str, Header()],
+                                    x_request_id: Annotated[str, Header()]):
 
+    if settings.integrations.rapidapi.use_rapidapi and settings.integrations.rapidapi.use_rapidapi_checking:
+        if not (x_rapidapi_host is not None and \
+                x_rapidapi_user is None and \
+                x_request_id is None and \
+                x_rapidapi_subscription is None and \
+                x_rapidapi_version is None and \
+                x_rapidapi_request_id):
+            return
+        else:
+            raise HTTPException(status_code=401, detail="Request not from rapidapi")
+        return
+    else:
+        return
 
 @router.post("/create")
 @version(1, 0)
@@ -14,7 +39,7 @@ async def main_create():
     return {"message": "main_create: Message sent"}
 
 
-@router.get("/read")
+@router.get("/read", dependencies=[Depends(is_request_from_rapid_api)])
 @version(1, 0)
 async def main_read():
     logger.debug('main read called')
